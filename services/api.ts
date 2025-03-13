c9ec6149296dae33a8739b0de11a71d5a977b1fc
Sylvanus-the-sorcerer <103826382+sepehrsylvanus@users.noreply.github.com>
2025-03-13 14:36:50 +0300

    movie details

--- services/api.ts
+++ services/api.ts
@@ -26,16 +26,25 @@ export const fetchMovies = async ({ query }: { query: string }) => {
   return data.results;
 };
 
-// const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
-// const options = {
-//   method: 'GET',
-//   headers: {
-//     accept: 'application/json',
-//     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhNzc0NjkyOGVjZjlmYjk3NmU3MzJhMmRiNTMxNThiOSIsIm5iZiI6MTY3MTIwNDE3OC40NDYsInN1YiI6IjYzOWM4ZDUyOTNkYjkyMDBjNjk2OWQ0MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ivSVfKOx3kPtqYYzMUbk1fMz1JrIxy9vulg-ekAiB_0'
-//   }
-// };
+export const fetchMovieDetails = async (
+  movieId: string
+): Promise<MovieDetails> => {
+  try {
+    const response = await fetch(
+      `${TMDB_CONFIG.BASE_URL}/movie/${movieId}?api_key=${TMDB_CONFIG.API_KEY}`,
+      {
+        method: "GET",
+        headers: TMDB_CONFIG.headers,
+      }
+    );
+    if (!response.ok) {
+      throw new Error("Failed to fetch movie details");
+    }
+    const data = await response.json();
 
-// fetch(url, options)
-//   .then(res => res.json())
-//   .then(json => console.log(json))
-//   .catch(err => console.error(err));
+    return data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
